f5a60f9e0bf22b9a7eeb176ff7ec6f5d66a6122d
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2025-07-06 16:43:38 -0400

    initial upload

---
+++ ThemeSwitcher/ThemeService.cs
@@ -0,0 +1,43 @@
+﻿public class ThemeService 
+{
+    public string teststring { get; set; } = "this came from a service, yay dependency injection !";
+
+    public bool isDarkMode { get; set; } = false;
+
+    public string mainBackground { get; set; } = "#DFD0B8";
+    public string altBackground { get; set; } = "#131010";
+
+    public string currentBackground { get; set; } = "#DFD0B8";
+
+    public string mainColumnColor { get; set; } = "#948979";
+    public string altColumnColor { get; set; } = "#393E46";
+    public string currentColumnColor { get; set; } = "#948979";
+
+    public string mainTextColor { get; set; } = "#E5E0D8";
+    public string altTextColor { get; set; } = "#DDE6ED";
+    public string currentTextColor { get; set; } = "#E5E0D8";
+
+
+    public void ToggleTheme() 
+    {
+        isDarkMode = !isDarkMode;
+        if (!isDarkMode)
+        {
+            this.currentBackground = mainBackground;
+            this.currentTextColor = mainTextColor;
+            this.currentColumnColor = mainColumnColor;
+        }
+        else 
+        {
+            this.currentBackground = altBackground;
+            this.currentTextColor = altTextColor;
+            this.currentColumnColor = altColumnColor;
+        }
+        Console.WriteLine(currentBackground);
+
+        
+    }
+
+
+
+}
