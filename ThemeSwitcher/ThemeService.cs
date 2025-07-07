d48fc384033ca774308b81e4aa1755e75ac8d1aa
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2025-07-07 14:44:26 -0400

    updated some thangs

--- ThemeSwitcher/ThemeService.cs
+++ ThemeSwitcher/ThemeService.cs
@@ -4,18 +4,18 @@
 
     public bool isDarkMode { get; set; } = false;
 
-    public string mainBackground { get; set; } = "#DFD0B8";
+    public string mainBackground { get; set; } = "#3F4E4F";
     public string altBackground { get; set; } = "#131010";
 
-    public string currentBackground { get; set; } = "#DFD0B8";
+    public string currentBackground { get; set; } = "#3F4E4F";
 
-    public string mainColumnColor { get; set; } = "#948979";
+    public string mainColumnColor { get; set; } = "#A27B5C";
     public string altColumnColor { get; set; } = "#393E46";
-    public string currentColumnColor { get; set; } = "#948979";
+    public string currentColumnColor { get; set; } = "#A27B5C";
 
-    public string mainTextColor { get; set; } = "#E5E0D8";
+    public string mainTextColor { get; set; } = "#DCD7C9";
     public string altTextColor { get; set; } = "#DDE6ED";
-    public string currentTextColor { get; set; } = "#E5E0D8";
+    public string currentTextColor { get; set; } = "#DCD7C9";
 
 
     public void ToggleTheme() 
